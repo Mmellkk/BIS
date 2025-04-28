fc190b41c93fe57f5c7733a80117b473a6e51208
Rhett Shanahan <rshanahan159@gmail.com>
2025-04-28 14:43:50 -0500

    Practice with For loops in Java

---
+++ AdvancedLanguages1/Java/ForClass.java
@@ -0,0 +1,20 @@
+package pkg;
+
+public class ForClass {
+
+	public static void main(String[] args) {
+		for(int i=0; i<9; i++) {
+			
+			
+			
+			if(i==7) {
+				continue;
+			}
+			
+			System.out.println("I number is " + i);
+			
+		}
+
+	}
+
+}
