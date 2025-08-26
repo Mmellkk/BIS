2ce39b0b162b304c4f457ee11a052db956e31b28
Rhett Shanahan <rshanahan159@gmail.com>
2025-08-25 19:46:31 -0500

    Update and rename HelloWorld.cbl to COBOL/HelloWorld.cbl

---
+++ COBOL/HelloWorld.cbl
@@ -0,0 +1,14 @@
+      *This is my first COBOL Program!
+       identification division.
+       program-id. FirstProgram.
+
+       environment division.
+
+       data division.
+
+       procedure division.
+           display ' '.
+           display 'Hello World'.
+           display ' '.
+           stop run.
+           
